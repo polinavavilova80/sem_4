adbeb7cdd1cde8eb0e74b19a90134671b7a426fa
polinavavilova80 <127448453+polinavavilova80@users.noreply.github.com>
2024-03-10 20:53:28 +0300

    Add files via upload

---
+++ shape.h
@@ -0,0 +1,10 @@
+//shape.h
+#pragma once
+#include <string>
+
+class Shape {
+public:
+    virtual void Scale(float scaleFactor) = 0;
+    virtual void ShowInfo() = 0;
+    virtual std::string GetName() = 0;
+};
